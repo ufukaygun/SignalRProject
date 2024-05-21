c05491103b4a49e966b0f8785160d163090eba44
ufukaygun <u.aygun869@gmail.com>
2024-05-21 14:54:20 +0300

    Data Access Layer Katmanı Tamamlandı

---
+++ SignalR.DataAccessLayer/EntityFramework/EfAboutDal.cs
@@ -0,0 +1,21 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.EntityFramework
+{
+    //Entity Framework içerisindeki classlarla interfaceleri ve genericr repoyu haberleştirmemiz gerekiyor.
+    //IAboutDAL dan miras almasının sebebi şu başka zaman Abouta özgü entityler gelebilir. o zaman devreye IAboutDAL bu giriyor
+    public class EfAboutDal : GenericRepository<About>, IAboutDAL
+    {
+        public EfAboutDal(SignalRContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ SignalR.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,51 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.Repositories
+{
+    //Repository Design Pattren kullanıldı.CRUD işlemlerini tekrar etmememi ve Generic bir yapı üzerinde tutmamı sağlayacak
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        //nesne örneği oluşturuldu
+        //Başka zaman yeni yazacapımız metodu önce  IGenericDal içerisinde yazıcalacak. Sonrada burada bu metodun içerisini doldurulacak.
+        private readonly SignalRContext _context;
+
+        public GenericRepository(SignalRContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(T entity)
+        {
+            _context.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+           _context.Remove(entity);
+           _context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+           return _context.Set<T>().ToList();
+        }
+
+        public T GetByID(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Update(T entity)
+        {
+           _context.Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
