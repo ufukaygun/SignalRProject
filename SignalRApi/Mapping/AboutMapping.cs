44cf4814452365abb53ced82d5da8be4b3c388b2
ufukaygun <u.aygun869@gmail.com>
2024-05-22 15:37:13 +0300

    AutoMapper eşleme işlemi yapıldı

---
+++ SignalRApi/Mapping/AboutMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.AboutDto;
+using SignalR.EntityLayer.Entities;
+namespace SignalRApi.Mapping
+{
+    public class AboutMapping : Profile
+    {
+        //.ReverseMap : hem result aboutla hemde about result la eşleşebilir
+        public AboutMapping()
+        {
+            CreateMap<About, ResultAboutDto>().ReverseMap();
+            CreateMap<About, CreateAboutDto>().ReverseMap();
+            CreateMap<About, GetAboutDto>().ReverseMap();
+            CreateMap<About, UpdateAboutDto>().ReverseMap();
+        }
+    }
+}
