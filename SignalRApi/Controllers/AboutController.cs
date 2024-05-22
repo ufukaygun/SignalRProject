dce27e14bb7e7a46e8ea30fda6219ee1a09eb763
ufukaygun <u.aygun869@gmail.com>
2024-05-22 21:45:21 +0300

    About Api Controller oluşturuldu

---
+++ SignalRApi/Controllers/AboutController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.AboutDto;
+using SignalR.EntityLayer.Entities;
+using System.Reflection;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        private readonly IAboutService _aboutService;
+        public AboutController(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+        [HttpGet]
+        public IActionResult AboutList()
+        {
+            var values = _aboutService.TGetAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateAbout(CreateAboutDto createAboutDto)
+        {
+            About about = new About()
+            {
+                Title = createAboutDto.Title,
+                Description = createAboutDto.Description,
+                ImageUrl = createAboutDto.ImageUrl
+            };
+            _aboutService.TAdd(about);
+            return Ok("Hakkımda Kısmı Başarılı Bir Şekilde Eklendi");
+        }
+        [HttpDelete]
+        public IActionResult DeleteAbout(int id)
+        {
+            var value = _aboutService.TGetByID(id);
+            _aboutService.TDelete(value);
+            return Ok("Hakkımda Alanı Silindi");
+        }
+        //Güncelleme için kullanılıyor
+        [HttpPut]
+        public IActionResult UpdateAbout(UpdateAboutDto updateAboutDto)
+        {
+            About about = new About()
+            {
+                AboutID = updateAboutDto.AboutID,
+                Title = updateAboutDto.Title,
+                Description = updateAboutDto.Description,
+                ImageUrl = updateAboutDto.ImageUrl
+            };
+            _aboutService.TUpdate(about);
+            return Ok("Hakkımda Alanı Güncellendi");
+        }
+        //id ye göre getirme işlemi
+        //Api de aynı Atribute aynı formatta birden fazla kullanılırsa hata verir.Hata vermemesi için içine isim yazıyoruz.
+        [HttpGet("GetAbout")]
+        public IActionResult GetAbout(int id)
+        {
+            var value = _aboutService.TGetByID(id);
+            return Ok(value);
+        }
+    }
+}
