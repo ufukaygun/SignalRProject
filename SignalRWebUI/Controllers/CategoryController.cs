bc32c4cc378f5a6338d165715654ed8c78027a32
ufukaygun <u.aygun869@gmail.com>
2024-05-29 16:16:09 +0300

    Kategorilerin güncellenme işlemi yapıldı

--- SignalRWebUI/Controllers/CategoryController.cs
+++ SignalRWebUI/Controllers/CategoryController.cs
@@ -62,5 +62,35 @@ namespace SignalRWebUI.Controllers
 			}
 			return View();
 		}
+		public async Task<IActionResult> UpdateCategory(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			//ilk önce güncellemek istediğimiz veriyi getiriyoruz.
+			var responseMessage = await client.GetAsync($"http://localhost:5205/api/Category/{id}");
+			if (responseMessage.IsSuccessStatusCode) 
+			{
+				//güncelleyeceğimiz veriyi Dto aracılığıyla çağırıyruz
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<UpdateCategoryDto>(jsonData);
+				return View(values);
+
+			}
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(updateCategoryDto);
+			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			//PutAsync güncelleme işlemi yapacak
+			var responseMessage = await client.PutAsync("http://localhost:5205/api/Category/", stringContent);
+			if (responseMessage.IsSuccessStatusCode) 
+			{
+				return RedirectToAction("Index"); 
+			}
+			return View();
+
+		}
     }
 }
