3e8dbca44af5ee18142c44328303096b06064c22
ufukaygun <u.aygun869@gmail.com>
2024-05-31 12:52:14 +0300

    Ürün Kategorileri Dropdownda listelendi

--- SignalRWebUI/Controllers/ProductController.cs
+++ SignalRWebUI/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using SignalRWebUI.Dtos.CategoryDtos;
 using SignalRWebUI.Dtos.ProductDtos;
 using System.Text;
 
@@ -31,8 +33,19 @@ namespace SignalRWebUI.Controllers
 			return View();
 		}
 		[HttpGet]
-		public IActionResult CreateProduct()
+		public async Task<IActionResult> CreateProduct()
 		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("http://localhost:5205/api/Category");
+			var jsonData = await responseMessage.Content.ReadAsStringAsync();
+			var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+			List<SelectListItem> values2 = (from x in values
+											select new SelectListItem
+											{
+												Text = x.CategoryName,
+												Value = x.CategoryID.ToString()
+											}).ToList();
+			ViewBag.v = values2;
 			return View();
 		}
 		[HttpPost]
