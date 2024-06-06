9b31f071960f1b6e50b96032ca81709546f262eb
ufukaygun <u.aygun869@gmail.com>
2024-06-06 15:07:53 +0300

    SignalR Hub Sınıfı

---
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRApi.Hubs
+{
+	//Hub bir sunucu görevi görecek.Biz dağıtım işlerini Hub sınıfı neyse onun üzerinden yapıcaz. Sınıfımız SignalRHub.
+	//Hub sınıfı aracılığıyla biz uygulamamızın servera olan kısmını tanıtmış olduk.
+	public class SignalRHub : Hub
+	{
+	}
+}
