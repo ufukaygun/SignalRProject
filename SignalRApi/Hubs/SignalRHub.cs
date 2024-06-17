77c62742f58e40bb56d82963ccc6b39140413eef
ufukaygun <u.aygun869@gmail.com>
2024-06-17 15:08:27 +0300

    Refactor(Düzenleme) yapıldı

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -17,25 +17,23 @@ namespace SignalRApi.Hubs
             _productService = productService;
         }
 
-        public async Task SendCategoryCount()
+        public async Task SendStatistic()
         {
             // Kategorilerin sayısını alıyoruz
             var value = _categoryService.TCategoryCount();
 
             // Bu metot ile gelen değeri client tarafına gönderiyoruz
             await Clients.All.SendAsync("ReceiveCategoryCount", value);
-        }
-        public async Task SendProductCount()
-        {
+
             var value2 = _productService.TProductCount();
             await Clients.All.SendAsync("ReceiveProductCount", value2);
-        }
-        public async Task ActivePassiveCategoryCount()
-        {
+
             var value3 = _categoryService.TActiveCategoryCount();
-            var value4 = _categoryService.TPassiveCategoryCount();
             await Clients.All.SendAsync("ReceiveActiveCategoryCount", value3);
+
+            var value4 = _categoryService.TPassiveCategoryCount();
             await Clients.All.SendAsync("ReceivePassiveCategoryCount", value4);
         }
+       
     }
 }
