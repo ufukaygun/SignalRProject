60de0a7d62f76f8dff66a8e4db08df44b311629d
ufukaygun <u.aygun869@gmail.com>
2024-06-19 14:45:44 +0300

    Yeni ProgressBarlar Eklendi

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -80,6 +80,12 @@ namespace SignalRApi.Hubs
         {
             var value = _moneyCaseService.TTotalMoneyCaseAmount();
             await Clients.All.SendAsync("ReceiveTotalMoneyCaseAmount", value.ToString("0.00")+ "₺");
+
+            var value2 = _orderService.TActiveOrderCount();
+            await Clients.All.SendAsync("ReceiveActiveOrderCount", value2);
+
+            var value3 = _menuTableService.TMenuTableCount();
+            await Clients.All.SendAsync("ReceiveMenuTableCount", value3);
         }
     }
 }
