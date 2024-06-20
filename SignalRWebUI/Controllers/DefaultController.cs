7f798af5c50973be912aceed84df793b79e075f2
ufukaygun <u.aygun869@gmail.com>
2024-06-20 15:00:58 +0300

    UI Layout Navbar ve Header oluşturuldu

---
+++ SignalRWebUI/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.Controllers
+{
+    //Anasayfa Olacak kısım
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SignalRWebUI/Controllers/UILayoutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.Controllers
+{
+    public class UILayoutController : Controller
+    {
+        //Kullanıcı arayüzü 
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
