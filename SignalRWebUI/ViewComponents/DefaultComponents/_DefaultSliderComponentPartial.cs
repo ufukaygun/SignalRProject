b6fb94a1e4aa57e9cc2c57870a4886985933881f
ufukaygun <u.aygun869@gmail.com>
2024-06-21 23:49:55 +0300

    Slider, Offer Layout Tamamlandı

---
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.DefaultComponents
+{
+    //Anasayfa Slider
+    public class _DefaultSliderComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
