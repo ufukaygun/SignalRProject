d649bb93f9802d7d246144588726fc0baa3bc911
ufukaygun <u.aygun869@gmail.com>
2024-07-06 18:11:59 +0300

    Testimonial Apı Consume edildi

--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultAboutComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultAboutComponentPartial.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.AboutDtos;
+using SignalRWebUI.Dtos.SliderDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
+    //Consume İşlemi
     public class _DefaultAboutComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        // _httpClientFactory adında field oluşturduk.
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DefaultAboutComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //İstemci oluşturduk
+            var client = _httpClientFactory.CreateClient();
+            //GetAsync verileri listelemek için kullanılan metod
+            var responseMessage = await client.GetAsync("http://localhost:5205/api/About");
+
+            //İşlem başarılı dönerse Json dan gelen içeriği string olarak oku
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            //values değişkenin içerisini oku. DeserializeObject Json bir datayı çözüp normal metne çevirir. Listeleme
+            //SeriAlize da bir metni Json formatına çevirir. Ekle Güncelle
+            var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+            return View(values);
         }
     }
 }
--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultTestimonialComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultTestimonialComponentPartial.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.TestimonialDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
     public class _DefaultTestimonialComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        // _httpClientFactory adında field oluşturduk.
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DefaultTestimonialComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //İstemci oluşturduk
+            var client = _httpClientFactory.CreateClient();
+            //GetAsync verileri listelemek için kullanılan metod
+            var responseMessage = await client.GetAsync("http://localhost:5205/api/Testimonial");
+
+            //İşlem başarılı dönerse Json dan gelen içeriği string olarak oku
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            //values değişkenin içerisini oku. DeserializeObject Json bir datayı çözüp normal metne çevirir. Listeleme
+            //SeriAlize da bir metni Json formatına çevirir. Ekle Güncelle
+            var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+            return View(values);
         }
     }
 }
