3e27bb8edd70e6bf65a4cd31fd4c2d8ce3c72403
ufukaygun <u.aygun869@gmail.com>
2024-07-06 15:59:09 +0300

    Slider Api Consume edildi

--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
@@ -1,13 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.SliderDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
     //Anasayfa Slider
-    public class _DefaultSliderComponentPartial:ViewComponent
+    public class _DefaultSliderComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        // _httpClientFactory adında field oluşturduk.
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DefaultSliderComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //İstemci oluşturduk
+            var client = _httpClientFactory.CreateClient();
+            //GetAsync verileri listelemek için kullanılan metod
+            var responseMessage = await client.GetAsync("http://localhost:5205/api/Sliders");
+            
+                //İşlem başarılı dönerse Json dan gelen içeriği string olarak oku
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                //values değişkenin içerisini oku. DeserializeObject Json bir datayı çözüp normal metne çevirir. Listeleme
+                //SeriAlize da bir metni Json formatına çevirir. Ekle Güncelle
+                var values = JsonConvert.DeserializeObject<List<ResultSliderDto>>(jsonData);
+                return View(values);            
         }
     }
 }
