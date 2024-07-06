0abfe47388a8ec03114b2a28eb7b0968012296a8
ufukaygun <u.aygun869@gmail.com>
2024-07-06 17:05:20 +0300

    Discount Offer Consume edildi

--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.DiscountDtos;
+using SignalRWebUI.Dtos.SliderDtos;
+using System.Net.Http;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
-    public class _DefaultOfferComponentPartial:ViewComponent
+    public class _DefaultOfferComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        // _httpClientFactory adında field oluşturduk.
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DefaultOfferComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //İstemci oluşturduk
+            var client = _httpClientFactory.CreateClient();
+            //GetAsync verileri listelemek için kullanılan metod
+            var responseMessage = await client.GetAsync("http://localhost:5205/api/Discount");
+
+            //İşlem başarılı dönerse Json dan gelen içeriği string olarak oku
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            //values değişkenin içerisini oku. DeserializeObject Json bir datayı çözüp normal metne çevirir. Listeleme
+            //SeriAlize da bir metni Json formatına çevirir. Ekle Güncelle
+            var values = JsonConvert.DeserializeObject<List<ResultDiscountDto>>(jsonData);
+            return View(values);
         }
     }
 }
