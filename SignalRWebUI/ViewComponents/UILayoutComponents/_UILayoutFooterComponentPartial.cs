545dbfddc10077642d0f27b2e722027fd2a6fdfa
ufukaygun <u.aygun869@gmail.com>
2024-07-07 15:31:49 +0300

    Footer Consume edildi

--- SignalRWebUI/ViewComponents/UILayoutComponents/_UILayoutFooterComponentPartial.cs
+++ SignalRWebUI/ViewComponents/UILayoutComponents/_UILayoutFooterComponentPartial.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.ContactDtos;
+
 
 namespace SignalRWebUI.ViewComponents.UILayoutComponents
 {
     public class _UILayoutFooterComponentPartial:ViewComponent
-    {
-        public IViewComponentResult Invoke()
+    { 
+        // _httpClientFactory adında field oluşturduk.
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _UILayoutFooterComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //İstemci oluşturduk
+            var client = _httpClientFactory.CreateClient();
+            //GetAsync verileri listelemek için kullanılan metod
+            var responseMessage = await client.GetAsync("http://localhost:5205/api/Contact");
+
+            //İşlem başarılı dönerse Json dan gelen içeriği string olarak oku
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            //values değişkenin içerisini oku. DeserializeObject Json bir datayı çözüp normal metne çevirir. Listeleme
+            //SeriAlize da bir metni Json formatına çevirir. Ekle Güncelle
+            var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+            return View(values);
         }
     }
 }
