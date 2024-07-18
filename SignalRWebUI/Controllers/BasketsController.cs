2d6c6e867a8a623e3cec085515547fc8a12df04b
ufukaygun <u.aygun869@gmail.com>
2024-07-18 13:10:28 +0300

    Sepet Ekranı Tasarımı yapıldı

---
+++ SignalRWebUI/Controllers/BasketsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.BasketDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class BasketsController : Controller
+    {
+        // _httpClientFactory adında field oluşturduk.
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BasketsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        //Asekron Çalışıyrouz Burada(Task)
+        public async Task<IActionResult> Index()
+        {
+            //İstemci oluşturduk
+            var client = _httpClientFactory.CreateClient();
+            //GetAsync verileri listelemek için kullanılan metod
+            var responseMessage = await client.GetAsync("http://localhost:5205/api/Basket");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                //İşlem başarılı dönerse Json dan gelen içeriği string olarak oku
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                //values değişkenin içerisini oku. DeserializeObject Json bir datayı çözüp normal metne çevirir. Listeleme
+                //SeriAlize da bir metni Json formatına çevirir. Ekle Güncelle
+                var values = JsonConvert.DeserializeObject<List<ResultBasketDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
