d8db7b6ec4973fd8a1644819837f48d6fcf0f5e1
ufukaygun <u.aygun869@gmail.com>
2024-07-20 17:46:57 +0300

    Rezervasyon yapma işlemi tamamlandı

--- SignalRWebUI/Controllers/BookATableController.cs
+++ SignalRWebUI/Controllers/BookATableController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.BookingDtos;
+using System.Text;
 
 namespace SignalRWebUI.Controllers
 {
     public class BookATableController : Controller
     {
+        // _httpClientFactory adında field oluşturduk.
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BookATableController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        //Asekron Çalışıyrouz Burada(Task)
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateBookingDto createBookingDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBookingDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5205/api/Booking", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index","Default");
+            }
+            return View();
+
         }
     }
 }
