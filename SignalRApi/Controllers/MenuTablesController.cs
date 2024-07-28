71828bba757d09b01634fe67b2b8e049cc8a327c
ufukaygun <u.aygun869@gmail.com>
2024-07-28 15:21:34 +0300

    ManuTable Api Alanları yazıldı

--- SignalRApi/Controllers/MenuTablesController.cs
+++ SignalRApi/Controllers/MenuTablesController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Migrations;
+using SignalR.DtoLayer.MenuTableDto;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Controllers
 {
@@ -19,5 +22,50 @@ namespace SignalRApi.Controllers
         {
             return Ok(_menuTableService.TMenuTableCount());
         }
-    }
+		[HttpGet]
+		public IActionResult MenuTableList()
+		{
+			var values = _menuTableService.TGetAll();
+			return Ok(values);
+		}
+		[HttpPost]
+		public IActionResult CreateMenuTable(CreateMenuTableDto createMenuTableDto )
+		{
+			MenuTable menuTable = new MenuTable()
+			{
+				Name = createMenuTableDto.Name,
+				Status = false
+			};
+			_menuTableService.TAdd(menuTable);
+			return Ok("Masa Kısmı Başarılı Bir Şekilde Eklendi");
+		}
+		[HttpDelete("{id}")]
+		public IActionResult DeleteMenuTable(int id)
+		{
+			var value = _menuTableService.TGetByID(id);
+			_menuTableService.TDelete(value);
+			return Ok("Masa Silindi");
+		}
+		//Güncelleme için kullanılıyor
+		[HttpPut]
+		public IActionResult UpdateMenuTable(UpdateMenuTableDto updateMenuTableDto)
+		{
+			MenuTable menuTable = new MenuTable()
+			{
+				Name = updateMenuTableDto.Name,
+				Status = false,
+				MenuTableID = updateMenuTableDto.MenuTableID
+			};
+			_menuTableService.TUpdate(menuTable);
+			return Ok("Masa Bilgileri Güncellendi");
+		}
+		//id ye göre getirme işlemi
+		//Api de aynı Atribute aynı formatta birden fazla kullanılırsa hata verir.Hata vermemesi için içine isim yazıyoruz.
+		[HttpGet("{id}")]
+		public IActionResult GetMenuTable(int id)
+		{
+			var value = _menuTableService.TGetByID(id);
+			return Ok(value);
+		}
+	}
 }
