83fc63d465fbd9c9b97237fd42725cd352309aac
ufukaygun <u.aygun869@gmail.com>
2024-07-29 15:38:00 +0300

    Menu Table Consume Backend

---
+++ SignalRWebUI/Controllers/MenuTablesController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.MenuTableDtos;
+using System.Text;
+
+namespace SignalRWebUI.Controllers
+{
+	public class MenuTablesController : Controller
+	{
+		// _httpClientFactory adında field oluşturduk.
+		private readonly IHttpClientFactory _httpClientFactory;
+		public MenuTablesController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+		//Asekron Çalışıyrouz Burada(Task)
+		public async Task<IActionResult> Index()
+		{
+			//İstemci oluşturduk
+			var client = _httpClientFactory.CreateClient();
+			//GetAsync verileri listelemek için kullanılan metod
+			var responseMessage = await client.GetAsync("http://localhost:5205/api/MenuTables");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				//İşlem başarılı dönerse Json dan gelen içeriği string olarak oku
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				//values değişkenin içerisini oku. DeserializeObject Json bir datayı çözüp normal metne çevirir. Listeleme
+				//SeriAlize da bir metni Json formatına çevirir. Ekle Güncelle
+				var values = JsonConvert.DeserializeObject<List<ResultMenuTableDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+		[HttpGet]
+		public IActionResult CreateMenuTable()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> CreateMenuTable(CreateMenuTableDto createMenuTableDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(createMenuTableDto);
+			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("http://localhost:5205/api/MenuTables", stringContent);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+
+		}
+		public async Task<IActionResult> DeleteMenuTable(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.DeleteAsync($"http://localhost:5205/api/MenuTables/{id}");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+		public async Task<IActionResult> UpdateMenuTable(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			//ilk önce güncellemek istediğimiz veriyi getiriyoruz.
+			var responseMessage = await client.GetAsync($"http://localhost:5205/api/MenuTables/{id}");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				//güncelleyeceğimiz veriyi Dto aracılığıyla çağırıyruz
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<UpdateMenuTableDto>(jsonData);
+				return View(values);
+
+			}
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> UpdateMenuTable(UpdateMenuTableDto updateMenuTableDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(updateMenuTableDto);
+			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			//PutAsync güncelleme işlemi yapacak
+			var responseMessage = await client.PutAsync("http://localhost:5205/api/MenuTables/", stringContent);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+
+		}
+	}
+}
