4398fd8730f23812ad1af7504adf9593d58f4678
ufukaygun <u.aygun869@gmail.com>
2024-08-03 15:51:19 +0300

    Register Backend Process

---
+++ SignalRWebUI/Controllers/RegisterController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.EntityLayer.Entities;
+using SignalRWebUI.Dtos.IdentityDtos;
+using System.Security.Policy;
+
+namespace SignalRWebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(RegisterDto registerDto)
+        {
+            var appUser = new AppUser()
+            {
+                Name = registerDto.Name,
+                Surname = registerDto.Surname,
+                Email = registerDto.mail,
+                UserName = registerDto.UserName
+            };
+            //CreateAsync yeni bir kullanıcı kayıt etmek için kullanılan metod
+            var result = await _userManager.CreateAsync(appUser, registerDto.Password);
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
---
+++ SignalRWebUI/Dtos/IdentityDtos/RegisterDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SignalRWebUI.Dtos.IdentityDtos
+{
+    //Kayıt olma metodu
+    public class RegisterDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string UserName { get; set; }
+        public string mail { get; set; }
+        public string Password { get; set; }
+    }
+}
