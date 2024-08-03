c44f1724e1356198e1d877fae1bc0f14701a1221
ufukaygun <u.aygun869@gmail.com>
2024-08-03 17:29:39 +0300

    Login Process

---
+++ SignalRWebUI/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.EntityLayer.Entities;
+using SignalRWebUI.Dtos.IdentityDtos;
+
+namespace SignalRWebUI.Controllers
+{
+	public class LoginController : Controller
+	{
+		private readonly SignInManager<AppUser> _signInManager;
+
+		public LoginController(SignInManager<AppUser> signInManager)
+		{
+			_signInManager = signInManager;
+		}
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Index(LoginDto loginDto)
+		{
+			//false veya true kullanıcıyı hatırlasın mı ? Beni Hatırla gibi
+			//2. false logout tarafı veritabanında yanlış girilen şifre sayısını tutan alan var. 5 ten fazla yanlış girildi bir süre giriş yapamazsınız gibi. 
+			var result = await _signInManager.PasswordSignInAsync(loginDto.UserName, loginDto.Password, false, false);
+			if (result.Succeeded) 
+			{
+				return RedirectToAction("Index", "Category");
+			}
+			return View();
+		}
+	}
+}
