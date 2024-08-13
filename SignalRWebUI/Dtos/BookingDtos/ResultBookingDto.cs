226b72fd3a85dfcb0c124821dd1d20a847899662
ufukaygun <u.aygun869@gmail.com>
2024-08-13 14:35:35 +0300

    Booking Revise

--- SignalRWebUI/Dtos/BookingDtos/ResultBookingDto.cs
+++ SignalRWebUI/Dtos/BookingDtos/ResultBookingDto.cs
@@ -19,5 +19,6 @@ namespace SignalRWebUI.Dtos.BookingDtos
         public string Mail { get; set; }
         public int PersonCount { get; set; }
         public DateTime Date { get; set; }
+        public string Description { get; set; }
     }
 }
