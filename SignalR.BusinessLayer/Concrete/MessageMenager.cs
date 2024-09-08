28f8477f084e5cc7fafa80dbf6ae02a5e940286d
ufukaygun <u.aygun869@gmail.com>
2024-09-08 16:22:25 +0300

    Message Class Architecture added

---
+++ SignalR.BusinessLayer/Concrete/MessageMenager.cs
@@ -0,0 +1,41 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+	public class MessageMenager : IMessageService
+	{
+		private readonly IMessageDal _messageDal;
+
+		public MessageMenager(IMessageDal messageDal)
+		{
+			_messageDal = messageDal;
+		}
+
+		public void TAdd(Message entity)
+		{
+			_messageDal.Add(entity);
+		}
+
+		public void TDelete(Message entity)
+		{
+			_messageDal.Delete(entity);	
+		}
+
+		public List<Message> TGetAll()
+		{
+			return _messageDal.GetAll();
+		}
+
+		public Message TGetByID(int id)
+		{
+			return _messageDal.GetByID(id);
+		}
+
+		public void TUpdate(Message entity)
+		{
+			_messageDal.Update(entity);
+		}
+	}
+}
---
+++ SignalRApi/Controllers/MessageController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.MessageDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MessageController : ControllerBase
+	{
+		private readonly IMessageService  _messageService;
+		public MessageController(IMessageService messageService )
+		{
+			_messageService = messageService;
+		}
+		[HttpGet]
+		public IActionResult MessageList()
+		{
+			var values = _messageService.TGetAll();
+			return Ok(values);
+		}
+		[HttpPost]
+		public IActionResult CreateMessage(CreateMessageDto createMessageDto)
+		{
+			Message message = new Message()
+			{
+				Mail = createMessageDto.Mail,
+				MessageContent = createMessageDto.MessageContent,
+				MessageSendDate = DateTime.Now,
+				NameSurname = createMessageDto.NameSurname,
+				Phone = createMessageDto.Phone,
+				Status = createMessageDto.Status,
+				Subject = createMessageDto.Subject
+			};
+			_messageService.TAdd(message);
+			return Ok("Mesaj Başarılı bir şekilde gönderildi");
+		}
+		[HttpDelete("{id}")]
+		public IActionResult DeleteMessage(int id)
+		{
+			var value = _messageService.TGetByID(id);
+			_messageService.TDelete(value);
+			return Ok("Mesaj Silindi");
+		}
+		//Güncelleme için kullanılıyor
+		[HttpPut]
+		public IActionResult UpdateMessage(UpdateMessageDto updateMessageDto)
+		{
+			Message message = new Message()
+			{
+				Mail = updateMessageDto.Mail,
+				MessageContent = updateMessageDto.MessageContent,
+				MessageSendDate = updateMessageDto.MessageSendDate,
+				NameSurname = updateMessageDto.NameSurname,
+				Phone = updateMessageDto.Phone,
+				Status = updateMessageDto.Status,
+				Subject = updateMessageDto.Subject,
+				MessageID = updateMessageDto.MessageID,
+			};
+			_messageService.TUpdate(message);
+			return Ok("Mesaj Alanı Güncellendi");
+		}
+		//id ye göre getirme işlemi
+		//Api de aynı Atribute aynı formatta birden fazla kullanılırsa hata verir.Hata vermemesi için içine isim yazıyoruz.
+		[HttpGet("{id}")]
+		public IActionResult GetMessage(int id)
+		{
+			var value = _messageService.TGetByID(id);
+			return Ok(value);
+		}
+	}
+}
