f1f6927a69fa4d368980f4b90b6340f3dee79ec6
ufukaygun <u.aygun869@gmail.com>
2024-09-10 18:57:24 +0300

    Social Media Links Listing

---
+++ SignalRWebUI/ViewComponents/UILayoutComponents/_UILayoutSocialMediaComponentPartial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.SocialMediaDtos;
+
+namespace SignalRWebUI.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutSocialMediaComponentPartial : ViewComponent
+    {
+        // _httpClientFactory adında field oluşturduk.
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _UILayoutSocialMediaComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        //Asekron Çalışıyrouz Burada(Task)
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //İstemci oluşturduk
+            var client = _httpClientFactory.CreateClient();
+            //GetAsync verileri listelemek için kullanılan metod
+            var responseMessage = await client.GetAsync("http://localhost:5205/api/SocialMedia");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                //İşlem başarılı dönerse Json dan gelen içeriği string olarak oku
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                //values değişkenin içerisini oku. DeserializeObject Json bir datayı çözüp normal metne çevirir. Listeleme
+                //SeriAlize da bir metni Json formatına çevirir. Ekle Güncelle
+                var values = JsonConvert.DeserializeObject<List<ResultSocialMediaDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
+
