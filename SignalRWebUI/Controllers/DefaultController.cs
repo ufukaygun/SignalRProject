487e2d0ae34935d374f56222a09df9b8433bdd05
ufukaygun <u.aygun869@gmail.com>
2024-09-10 18:02:27 +0300

    Send Message Consume Process

--- SignalRWebUI/Controllers/DefaultController.cs
+++ SignalRWebUI/Controllers/DefaultController.cs
@@ -1,13 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalR.DtoLayer.MessageDto;
+using System.Net.Http;
+using System.Text;
 
 namespace SignalRWebUI.Controllers
 {
     //Anasayfa Olacak kısım
     public class DefaultController : Controller
-    {
-        public IActionResult Index()
+	{
+		// _httpClientFactory adında field oluşturduk.
+		private readonly IHttpClientFactory _httpClientFactory;
+		public DefaultController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+		public IActionResult Index()
         {
             return View();
         }
+        [HttpGet]
+        public PartialViewResult SendMessage()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(CreateMessageDto createMessageDto)
+		{
+			//consume işlemi
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(createMessageDto);
+			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("http://localhost:5205/api/Message", stringContent);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
     }
 }
