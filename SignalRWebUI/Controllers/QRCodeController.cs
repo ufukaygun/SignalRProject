ca8768f1cc25046343f35d2a0ca23ba549bbdcf3
ufukaygun <u.aygun869@gmail.com>
2024-09-14 01:30:02 +0300

    QR Code Controller

---
+++ SignalRWebUI/Controllers/QRCodeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace SignalRWebUI.Controllers
+{
+    public class QRCodeController : Controller
+    {
+        public IActionResult Index(string value)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                QRCodeGenerator createQRCode = new QRCodeGenerator();
+                QRCodeGenerator.QRCode squareCode = createQRCode.CreateQrCode(value, QRCodeGenerator.ECCLevel.Q);
+                //Bitmap QRCOde image oluşturur
+                using (Bitmap image = squareCode.GetGraphic(10))
+                {
+                    image.Save(memoryStream, ImageFormat.Png);
+                    ViewBag.QRCodeImage = "data:Image/png;base64" + Convert.ToBase64String(memoryStream.ToArray());
+                }
+            }
+            return View();
+        }
+    }
+}
