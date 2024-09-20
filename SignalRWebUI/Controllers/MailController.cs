0384a0192f567301025b7a40f5d6a8d9399b633c
ufukaygun <u.aygun869@gmail.com>
2024-09-20 17:06:57 +0300

    Sending Mail Backend and NetCore.MailKit Package Install

---
+++ SignalRWebUI/Controllers/MailController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MailKit.Net.Smtp;
+using SignalRWebUI.Dtos.MailDtos;
+
+namespace SignalRWebUI.Controllers
+{
+	//NetCore.MailKit paketi yüklendi
+	public class MailController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Index(CreateMailDto createMailDto)
+		{
+			MimeMessage mimeMessage = new MimeMessage();
+
+			//MailboxAddress sınıfından nesne örneği alındı From ifadesi mailin kimden gideceğini gösterir
+			MailboxAddress mailboxAddressFrom = new MailboxAddress("SignalR Rezervasyon", "mail adresi");
+			mimeMessage.From.Add(mailboxAddressFrom);
+
+			//mailboxAddressTo buda mailin kime gideceği
+			MailboxAddress mailboxAddressTo = new MailboxAddress("User", createMailDto.ReceiverMail);
+			mimeMessage.To.Add(mailboxAddressTo);
+
+			//mail içeriği
+			var bodyBuilder = new BodyBuilder();
+			bodyBuilder.TextBody = createMailDto.Body;
+			mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+			mimeMessage.Subject = createMailDto.Subject;
+
+			//Şifreleme
+			SmtpClient client = new SmtpClient();
+			client.Connect("smtp.gmail.com", 587, false);
+			client.Authenticate("mail adresi", "key");
+
+			client.Send(mimeMessage);
+			client.Disconnect(true);
+
+			return RedirectToAction("Index", "Category");
+		}
+	}
+}
